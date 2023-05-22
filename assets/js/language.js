178269e8b307297438b54aeef2f0f1b2e220faaa
phuthinhnguyen <phuthinhnguyen1101@gmail.com>
2023-05-22 19:35:56 +0700

    sdf

---
+++ assets/js/language.js
@@ -0,0 +1,15 @@
+
+$(':root').attr('lang', localStorage["language"]);
+function eng(){
+    localStorage["language"] = "en";
+    $(':root').attr('lang', localStorage["language"]);
+}
+function vi(){
+    localStorage["language"] = "vi";
+    $(':root').attr('lang', localStorage["language"]);
+}
+function fr(){
+    localStorage["language"] = "fr";
+    $(':root').attr('lang', localStorage["language"]);
+}
+
